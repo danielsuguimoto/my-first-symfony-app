4eb44df4ededc5fc081770fa5220233c3d70ff8f
Daniel Suguimoto <danielsuguimoto@gmail.com>
2018-09-06 18:30:09 -0300

    Sessions

---
+++ src/Event/LocaleSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+/**
+ * Description of LocaleSubscriber
+ *
+ * @author partnerfusion
+ */
+class LocaleSubscriber implements EventSubscriberInterface {
+
+    /**
+     * @var string
+     */
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en') {
+        
+        $this->defaultLocale = $defaultLocale;
+    }
+    
+    public static function getSubscribedEvents() {
+        return [
+            KernelEvents::REQUEST => [
+                [
+                    'onKernelRequest',
+                    20
+                ],
+            ],
+        ];
+    }
+    
+    public function onKernelRequest(GetResponseEvent $event) {
+        $request = $event->getRequest();
+        
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+        
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+}
